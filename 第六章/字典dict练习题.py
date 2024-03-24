0fff853fa370011caa0cef99498e3170d71c3a1f
S-Planet-0 <371727000@qq.com>
2024-03-24 09:20:40 +0800

    Changes

---
+++ 第六章/字典dict练习题.py
@@ -0,0 +1,8 @@
+fruits = {"小明": "apple", "小张": "banana", "小白": "orange",}
+print(fruits)
+
+# 更新数据
+fruits["小王"] = "pear"
+fruits["小李"] = "cherry"
+fruits["小白"] = "strawberry"
+print("更新数据后：", fruits)
